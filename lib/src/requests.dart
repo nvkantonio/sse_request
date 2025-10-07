4c5e97dea1c86ac10ef0e83b1936ea783f07742b
nvkantonio <nvkantonio@gmail.com>
2025-10-07 13:50:46 +0200

    feat update: added sendStreamed methods shorthands

--- lib/src/requests.dart
+++ lib/src/requests.dart
@@ -28,3 +28,31 @@ Stream<Map<String, dynamic>> sseRequestPostStream({
     body: body,
   ).getStream(subName);
 }
+
+Future<Stream<Map<String, dynamic>>> sseRequestGetSendStreamed({
+  required Uri uri,
+  Client? client,
+  Map<String, String>? headers,
+  Encoding? encoding,
+}) {
+  return SseRequest.get(
+    uri: uri,
+    headers: headers,
+    encoding: encoding,
+  ).sendStreamed(client);
+}
+
+Future<Stream<Map<String, dynamic>>> sseRequestPostSendStreamed({
+  required Uri uri,
+  Client? client,
+  Map<String, String>? headers,
+  Map<String, dynamic>? body,
+  Encoding? encoding,
+}) {
+  return SseRequest.post(
+    uri: uri,
+    headers: headers,
+    body: body,
+    encoding: encoding,
+  ).sendStreamed(client);
+}
